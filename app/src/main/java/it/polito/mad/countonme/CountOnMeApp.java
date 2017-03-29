6e94b00065096b23d649becfc7156901085bd349
Francesco Bruno <francesco.bruno@egluetechnologies.com>
2017-03-29 21:39:28 +0200

    add custom application class

---
+++ app/src/main/java/it/polito/mad/countonme/CountOnMeApp.java
@@ -0,0 +1,12 @@
+package it.polito.mad.countonme;
+
+import android.app.Application;
+
+/**
+ * Exended application class for data global sharing
+ * Created by francescobruno on 29/03/17.
+ */
+
+public class CountOnMeApp extends Application {
+
+}
