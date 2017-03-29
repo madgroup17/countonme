8bb62826851f8c6398d72e58df26f691acbfd3a8
Francesco Bruno <francesco.bruno@egluetechnologies.com>
2017-03-29 21:18:59 +0200

    add Butterknife as dependency

--- app/src/main/java/it/polito/mad/countonme/MainActivity.java
+++ app/src/main/java/it/polito/mad/countonme/MainActivity.java
@@ -2,12 +2,19 @@ package it.polito.mad.countonme;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends Activity {
+    @BindView(R.id.tv) TextView title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
     }
 }
