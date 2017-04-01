bc241cc3b49522f78e00f65fcd4e52ee29e77399
madgroup172017 <madgroup17@gmail.com>
2017-04-01 16:50:58 +0200

    add expense list management

--- app/src/main/java/it/polito/mad/countonme/ExpenseListFragment.java
+++ app/src/main/java/it/polito/mad/countonme/ExpenseListFragment.java
@@ -10,10 +10,11 @@ import android.view.ViewGroup;
  * Created by LinaMaria on 31/03/2017.
  */
 
+
+
 public class ExpenseListFragment extends ListFragment {
     @Override
-    public View onCreateView(LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.expense_list_fragment, container, false);
         return view;
     }
