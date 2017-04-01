bd2ee4d2f51e98df7e569b0ffe22ba630dc3aeb8
madgroup172017 <madgroup17@gmail.com>
2017-04-01 16:56:32 +0200

    fix ignore file

--- app/src/main/java/it/polito/mad/countonme/SharingActivity.java
+++ app/src/main/java/it/polito/mad/countonme/SharingActivity.java
@@ -4,18 +4,26 @@ import android.app.Activity;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class SharingActivity extends Activity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sharing);
+        //setContentView(R.layout.activity_sharing);
+        setContentView(R.layout.expense_list);
+        //Button b = (Button)findViewById(R.id.AddExpense);
 
         FragmentManager fm = getFragmentManager();
         FragmentTransaction fragmentTransaction = fm.beginTransaction();
 
-        SharingsListFragment sharingListFragment = new SharingsListFragment();
+        /*SharingsListFragment sharingListFragment = new SharingsListFragment();
         fragmentTransaction.replace(android.R.id.content, sharingListFragment );
+        fragmentTransaction.commit();*/
+
+        ExpenseListFragment expenseListFragment = new ExpenseListFragment();
+        fragmentTransaction.replace(android.R.id.content, expenseListFragment );
         fragmentTransaction.commit();
 
     }
