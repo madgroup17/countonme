9ce295501a5abe64d0296d22af78510df66bf3b9
Francesco Bruno <francesco.bruno@egluetechnologies.com>
2017-04-05 22:49:13 +0200

    remove unused imports

--- app/src/main/java/it/polito/mad/countonme/SharingActivity.java
+++ app/src/main/java/it/polito/mad/countonme/SharingActivity.java
@@ -7,11 +7,8 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.Toast;
 
-import it.polito.mad.countonme.database.DataManager;
-import it.polito.mad.countonme.exceptions.InvalidDataException;
 import it.polito.mad.countonme.interfaces.IActionReportBack;
 import it.polito.mad.countonme.models.ReportBackAction;
 
