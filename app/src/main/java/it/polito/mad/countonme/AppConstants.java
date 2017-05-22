c310f1e5fb9f5ee26cfec05fdb0e16638c3c957c
Khatereh <khatereh.yamini@gmail.com>
2017-05-22 20:32:36 +0200

    Edit Menu

--- app/src/main/java/it/polito/mad/countonme/AppConstants.java
+++ app/src/main/java/it/polito/mad/countonme/AppConstants.java
@@ -8,14 +8,18 @@ public class AppConstants {
 
     public final static String SHARING_ACTIVITY_KEY = "Sharing_Activity_Key";
 
-    public final static String EXPENSE_KEY          = "Expense_Key";
+    public final static String EXPENSE_KEY = "Expense_Key";
 
-    public final static String USER_KEY             = "User_Key";
-
-
-    public final static String SAVE_STATE_KEY_SAVED  = "Saved";
+    public final static String USER_KEY = "User_Key";
 
 
-    public final static int GET_IMAGE_REQUEST       = 1;
+    public final static String SAVE_STATE_KEY_SAVED = "Saved";
+
+
+    public final static int GET_IMAGE_REQUEST = 1;
+
+    public final static String EDIT_MODE = "Edit";
+    public final static String NEW_MODE = "New";
+    public final static String MODE = "Mode";
 
 }
